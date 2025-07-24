83c8dbd67e6a8c8bef0068a1be14211eb872fff4
nisyanth <n3thanab@uwaterloo.ca>
2025-07-24 18:00:32 -0400

    Updated Questionnaire navigation

--- app/src/main/java/com/example/schedula/ui/QuestionnaireMenuScreen.kt
+++ app/src/main/java/com/example/schedula/ui/QuestionnaireMenuScreen.kt
@@ -1,11 +1,5 @@
 package com.example.schedula.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.CardDefaults
-import androidx.compose.ui.text.font.FontWeight
-import androidx.navigation.NavController
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -13,23 +7,42 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.rememberNavController
-
+import androidx.navigation.NavController
+import com.example.schedula.ui.components.BottomNavBar
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun QuestionnaireMenuScreen(navController: NavController) {
     val cardColor = Color(0xFFE6DEF6)
 
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp))
-        {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Edit Questionnaire") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigate("profile") }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            BottomNavBar(currentScreen = "questionsMenu", navController = navController)
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
             Spacer(modifier = Modifier.height(12.dp))
 
             Text(
@@ -42,19 +55,23 @@ fun QuestionnaireMenuScreen(navController: NavController) {
             Spacer(modifier = Modifier.height(24.dp))
 
             Card(
-                modifier = Modifier.fillMaxWidth().clickable { navController.navigate("lifestyleQuestionnaire")},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { navController.navigate("lifestyleQuestionnaire") },
                 colors = CardDefaults.cardColors(containerColor = cardColor),
                 shape = RoundedCornerShape(12.dp)
             ) {
                 Text(
                     text = "Lifestyle 1",
                     modifier = Modifier.padding(16.dp),
-                    fontSize =  18.sp
+                    fontSize = 18.sp
                 )
             }
 
             Card(
-                modifier = Modifier.fillMaxWidth().clickable { navController.navigate("extendedQuestionnaire") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { navController.navigate("extendedQuestionnaire") },
                 colors = CardDefaults.cardColors(containerColor = cardColor),
                 shape = RoundedCornerShape(12.dp)
             ) {
@@ -66,7 +83,9 @@ fun QuestionnaireMenuScreen(navController: NavController) {
             }
 
             Card(
-                modifier = Modifier.fillMaxWidth().clickable { navController.navigate("hobbiesQuestionnaire") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { navController.navigate("hobbiesQuestionnaire") },
                 colors = CardDefaults.cardColors(containerColor = cardColor),
                 shape = RoundedCornerShape(12.dp)
             ) {
@@ -78,7 +97,9 @@ fun QuestionnaireMenuScreen(navController: NavController) {
             }
 
             Card(
-                modifier = Modifier.fillMaxWidth().clickable { navController.navigate("customRoutineQuestionnaire") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { navController.navigate("customRoutineQuestionnaire") },
                 colors = CardDefaults.cardColors(containerColor = cardColor),
                 shape = RoundedCornerShape(12.dp)
             ) {
@@ -88,7 +109,6 @@ fun QuestionnaireMenuScreen(navController: NavController) {
                     fontSize = 18.sp
                 )
             }
-
         }
-
+    }
 }
